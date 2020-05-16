5d8086bd6973b3da0ef75a3213fafdce0b52a730
Adam Morris <amorris2003@gmail.com>
2020-05-16 17:30:53 +1000

    remove dist

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,11 @@
 for file in src/life/*; do
   filename=$(basename -s ".purs" $file)
   modulename=$(head -n 1 "$file" | sed -n 's/module \(.[^ ]*\).*/\1/p')
+
+  mkdir dist/life
+  mkdir dist/life/$filename
+
+  cat dist/life.html > dist/life/$filename/index.html
+
   spago bundle-app -m $modulename -t dist/life/$filename/index.js
 done
