f70fdce27582d3d496002d7e9d85caff93dd37f1
Adam Morris <amorris2003@gmail.com>
2020-05-16 11:31:53 +1000

    array

--- src/Main.purs
+++ src/Main.purs
@@ -1,20 +1,16 @@
 module Main where
 
-import Prelude (Unit, bind, map, (/=))
+import Data.TraversableWithIndex (traverseWithIndex)
 
-import Effect (Effect)
-import Graphics.Canvas (getCanvasElementById, getContext2D)
-import Partial.Unsafe (unsafePartial)
 import Data.Maybe (fromJust)
-import Effect.Console (log)
-
-main :: Effect Unit
-main = do
-  mcanvas <- getCanvasElementById "canvas"
-  let canvas = unsafePartial (fromJust mcanvas)
-  ctx <- getContext2D canvas
-  
-  log "test"
+import Effect (Effect)
+import Graphics.Canvas (Context2D, getCanvasElementById, getContext2D)
+import Graphics.Drawing (FillStyle, circle, fillColor, filled, render)
+import Partial.Unsafe (unsafePartial)
+import Prelude (Unit, bind, map, ($), (*), (+), (/=))
+import Color.Scale (sample)
+import Color.Scale.Perceptual (magma)
+import Data.Int (toNumber)
 
 startingPosition :: Array (Array Boolean)
 startingPosition = map (map (_ /= 0))
@@ -23,3 +19,19 @@ startingPosition = map (map (_ /= 0))
     [0, 1, 0],
     [0, 0, 1]
   ]
+
+main :: Effect (Array (Array Unit))
+main = do
+  mcanvas <- getCanvasElementById "canvas"
+  let canvas = unsafePartial (fromJust mcanvas)
+  ctx <- getContext2D canvas
+
+  traverseWithIndex (\i a -> traverseWithIndex (drawCell ctx i) a ) startingPosition
+
+getCellStyle :: Boolean -> FillStyle
+getCellStyle x = (fillColor $ sample magma 3.0)
+
+drawCell :: Context2D -> Int -> Int -> Boolean -> Effect Unit
+drawCell ctx rowIndex colIndex isActive = do
+  render ctx $
+    filled (getCellStyle isActive) (circle (50.0 * (toNumber rowIndex) + 25.0) (50.0 * (toNumber colIndex) + 25.0) 25.0)
