86c39279904ba6ce5e76b2621472c83a30f1bf30
Adam Morris <amorris2003@gmail.com>
2020-05-16 17:37:11 +1000

    new build script

--- src/build.sh
+++ src/build.sh
@@ -2,19 +2,20 @@ for file in src/life/*; do
   filename=$(basename -s ".purs" $file)
   modulename=$(head -n 1 "$file" | sed -n 's/module \(.[^ ]*\).*/\1/p')
 
-  if [ ! -d "dist"]; then
+  if [ ! -d "dist" ]; then
     mkdir dist
   fi
 
-  if [ ! -d "dist/life"]; then
+  if [ ! -d "dist/life" ]; then
     mkdir dist/life
   fi
 
-  if [ ! -d "dist/life/$filename"]; then
+  if [ ! -d "dist/life/$filename" ]; then
     mkdir dist/life/$filename
   fi
 
   cat src/site/life.html > dist/life/$filename/index.html
+  cat src/site/index.html > dist/index.html
 
   spago bundle-app -m $modulename -t dist/life/$filename/index.js
 done
