b6f891fda2f1b331330936f55eec0434393fdf68
Adam Morris <amorris2003@gmail.com>
2020-05-16 11:00:11 +1000

    drawing

--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,25 @@
 module Main where
 
-import Prelude
+import Prelude (Unit, bind, map, (/=))
 
 import Effect (Effect)
+import Graphics.Canvas (getCanvasElementById, getContext2D)
+import Partial.Unsafe (unsafePartial)
+import Data.Maybe (fromJust)
 import Effect.Console (log)
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  mcanvas <- getCanvasElementById "canvas"
+  let canvas = unsafePartial (fromJust mcanvas)
+  ctx <- getContext2D canvas
+  
+  log "test"
+
+startingPosition :: Array (Array Boolean)
+startingPosition = map (map (_ /= 0))
+  [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]
+  ]
