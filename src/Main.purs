4d434cce1863d9057be1619abb6e81748abd9a09
Adam Morris <amorris2003@gmail.com>
2020-05-16 11:58:14 +1000

    clean code

--- src/Main.purs
+++ src/Main.purs
@@ -7,44 +7,44 @@ import Effect (Effect)
 import Graphics.Canvas (Context2D, getCanvasElementById, getContext2D)
 import Graphics.Drawing (FillStyle, rectangle, fillColor, filled, render)
 import Partial.Unsafe (unsafePartial)
-import Prelude (Unit, bind, map, ($), (*), (+), (/=), (-))
+import Prelude (Unit, bind, map, ($), (*), (+), (/=), (-), discard, mul)
 import Color
 import Color.Scale (sample)
 import Color.Scale.Perceptual (magma)
 import Data.Int (toNumber)
+import Effect.Console
 
 startingPosition :: Array (Array Int)
 startingPosition =
   [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   ]
 
 main :: Effect (Array (Array Unit))
 main = do
   mcanvas <- getCanvasElementById "canvas"
-  let canvas = unsafePartial (fromJust mcanvas)
-  ctx <- getContext2D canvas
+  ctx <- getContext2D $ unsafePartial $ fromJust mcanvas
 
   traverseWithIndex (\i a -> traverseWithIndex (drawCell ctx i) a ) startingPosition
 
 getCellStyle :: Int -> FillStyle
 getCellStyle x = fillColor $ rgba active active active 1.0
-  where active = ((1 - x) * 255)
+  where active = (1 - x) * 255
 
 drawCell :: Context2D -> Int -> Int -> Int -> Effect Unit
 drawCell ctx rowIndex colIndex isActive = do
   render ctx $
-    filled (getCellStyle isActive) (rectangle (cellSize * (toNumber rowIndex)) (cellSize * (toNumber colIndex)) (cellSize - spacing) (cellSize - spacing))
+    filled (getCellStyle isActive) $
+      rectangle (mul cellSize $ toNumber rowIndex) (mul cellSize $ toNumber colIndex) (cellSize - spacing) (cellSize - spacing)
   where cellSize = 50.0
         spacing = 1.0
